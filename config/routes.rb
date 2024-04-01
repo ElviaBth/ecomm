a5f08abd894ad647251b6e209bc1c8d7b0a570b7
elviabth <ejbenedith@gmail.com>
2024-04-01 17:26:01 +0200

    create categories scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
   devise_for :admins
   #devise_for :installs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
